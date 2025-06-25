964dabe8359f215cebecbaa44455c333aea8700b
audranALU <a.ndayiseng@alustudent.com>
2025-06-25 14:08:29 +0200

    fnal mhmwrk

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (!name || !email || !message) {
+    alert("Please fill out all fields.");
+    return;
+  }
+
+  alert("Thanks for reaching out, Aurdan will reply soon!");
+  this.reset();
+});
